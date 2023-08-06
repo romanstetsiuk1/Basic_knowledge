6e829a55f1cff98fc8075ebf012eab3a3027bc1e
Roman Stetsiuk <romansteciuk021@gmail.com>
2023-08-06 23:43:40 +0200

    Instrukcaja warunkowa IF

--- Basic_knowledge/First_program.cpp
+++ Basic_knowledge/First_program.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+	/*
 	// pierwszy program :)
 	cout << "Witamy na pokladzie.\n\n";
 
@@ -32,6 +33,26 @@ int main()
 	cantymetry = cale * przelicznik_cale_na_cantymetry;
 
 	cout << "\n" << cale << " cale to jest: " << cantymetry << " cantymetrow.\n\n";
+
+	*/
+	// -- Rozdzial II. Instrukcje warunkowe. --
+	// Instrukcaja warunkowa IF
+	int wys, punkty_karne;		// zmienne tego samego typu mozna wpisywac przez przecinek
+
+	cout << "Na jakiej wysokosci lecimy? [w metrach]: ";
+	cin >> wys;
+
+	if (wys < 500) {
+		cout << "\n" << wys << " metrow to za nisko! \n";
+		punkty_karne = 1;
+	}
+	else {
+		cout << "\nNa wysokosci " << wys << " metrow jestes juz bezpieczny.\n";
+		punkty_karne = 0;
+	}
+
+	cout << "Masz " << punkty_karne << " punktow karnych.\n";
+	if (punkty_karne) cout << "Popraw sie!\n\n";
 
 
 }
